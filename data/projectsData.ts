9b9e358e1f50add23952cacdbf6e2896abed9c7d
Robert Tucker <robertwtucker@gmail.com>
2024-04-19 20:50:59 -0400

    chore: migrate blog to nextjs (#17)
    * chore: init for next-js (create-next-app)
    * chore: removed hugo-specific files
    * chore: merged timlrx/tailwind-nextjs-starter-blog
    * chore: migrate blog content
    * build: switch netlify config to nextjs
    * docs: update readme

---
+++ data/projectsData.ts
@@ -0,0 +1,18 @@
+interface Project {
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
+}
+
+const projectsData: Project[] = [
+  {
+    title: 'Cheat Sheet for Dash',
+    description: `I created a cheat sheet for Oh-My-Zsh's kubectl plugin. If you
+    code on a Mac, you should check out Dash!`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: 'https://kapeli.com/cheat_sheets/Oh-My-Zsh_Kubectl.docset/Contents/Resources/Documents/index',
+  },
+]
+
+export default projectsData
