86986959fbd0c64ac925046aea4fb5a4dacd2596
Robert Tucker <robertwtucker@gmail.com>
2024-11-22 21:52:12 -0500

    docs: add kfinder project (#144)
    * docs: add kfinder project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,10 +7,16 @@ interface Project {
 
 const projectsData: Project[] = [
   {
+    title: 'KFinder for iOS',
+    description: `My 'perpetual side-project' is now available on the App Store!`,
+    imgSrc: '/static/images/iphone.jpg',
+    href: 'https://kfinderapp.com',
+  },
+  {
     title: 'Cheat Sheet for Dash',
     description: `I created a cheat sheet for Oh-My-Zsh's kubectl plugin. If you
     code on a Mac, you should check out Dash!`,
-    imgSrc: '/static/images/time-machine.jpg',
+    imgSrc: '/static/images/devops.jpg',
     href: 'https://kapeli.com/cheat_sheets/Oh-My-Zsh_Kubectl.docset/Contents/Resources/Documents/index',
   },
 ]
